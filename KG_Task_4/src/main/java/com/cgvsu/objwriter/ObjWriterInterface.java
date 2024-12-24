ee304ac0ec531bf3fc58a8ff4b5938de06ea7070
Духанина Диана <90418779+DDukhanina@users.noreply.github.com>
2024-12-25 02:02:28 +0300

    null fixed

--- KG_Task_4/src/main/java/com/cgvsu/objwriter/ObjWriterInterface.java
+++
@@ -1,9 +0,0 @@
-package com.cgvsu;
-
-import com.cgvsu.model.Model;
-
-import java.io.IOException;
-
-public interface ObjWriterInterface {
-    void write(Model model, String fileName) throws IOException;
-}
--- KG_Task_4/src/main/java/com/cgvsu/render_engine/RenderStyle.java
+++
@@ -1,7 +0,0 @@
-package com.cgvsu.render_engine;
-
-public enum RenderStyle {
-    Polygonal_Grid,
-    Color_Fill,
-    Texture
-}
